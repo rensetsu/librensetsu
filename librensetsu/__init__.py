933be4be0ab36017a32d609a6f1c93447b2fe388
Sultan Iskandar Maulana <sultan.ridita@gmail.com>
2024-04-08 09:37:45 +0000

    Make the lib installable with setuptools

---
+++ librensetsu/__init__.py
@@ -0,0 +1,12 @@
+from .models import Date, Iso31661A2, PictureUrls, RelationMaps, BasicMediaInfo, MediaInfo
+
+__version__ = '0.1.0'
+
+__all__ = [
+    'Date',
+    'Iso31661A2',
+    'PictureUrls',
+    'RelationMaps',
+    'BasicMediaInfo',
+    'MediaInfo'
+]
