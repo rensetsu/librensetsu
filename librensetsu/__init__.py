eb64c136a3612a6036a96ccdea747e650ec31456
Sultan Iskandar Maulana <sultan.ridita@gmail.com>
2024-04-10 15:47:49 +0000

    Bump package version

--- librensetsu/__init__.py
+++ librensetsu/__init__.py
@@ -2,22 +2,23 @@ from .const import GITHUB_EVENT_NAME, GITHUB_WORKSPACE, IS_GITHUB_WORKFLOW, IS_G
 from .datadownloader import Downloader
 from .download_unidic import download_unidic
 from .humanclock import convert_float_to_time
-from .models import Date, PictureUrls, RelationMaps, BasicMediaInfo, MediaInfo
+from .models import Date, PictureUrls, RelationMaps, MediaInfo, ConventionalMapping, IdSlugPair
 from .prettyprint import Platform, PrettyPrint, Status, translate_hex_to_rgb
 from .slugify import slugify
 from .transliterate import char_maps, transliterate_no_accent
 
-__version__ = '0.1.3'
+__version__ = '0.1.4'
 
 __all__ = [
-    'BasicMediaInfo',
     'char_maps',
+    'ConventionalMapping',
     'convert_float_to_time',
     'Date',
     'download_unidic',
     'Downloader',
     'GITHUB_EVENT_NAME',
     'GITHUB_WORKSPACE',
+    'IdSlugPair',
     'IS_GITHUB_WORKFLOW_DISPATCH',
     'IS_GITHUB_WORKFLOW',
     'MediaInfo',
